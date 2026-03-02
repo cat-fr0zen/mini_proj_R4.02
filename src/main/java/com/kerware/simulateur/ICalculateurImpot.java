6120dd2077b031a9297c823fee9fd77b904cf1c1
picots <solal.picot@gmail.com>
2026-03-02 11:35:08 +0100

    [FEATURE] ajout de  DeclarantSeulException

--- src/main/java/com/kerware/simulateur/ICalculateurImpot.java
+++ src/main/java/com/kerware/simulateur/ICalculateurImpot.java
@@ -1,9 +1,11 @@
 package com.kerware.simulateur;
 
+import com.kerware.simulateur.exception.DeclarantSeulException;
+
 public interface ICalculateurImpot {
 
     public void setRevenusNetDeclarant1( int rn );
-    public void setRevenusNetDeclarant2( int rn );
+    public void setRevenusNetDeclarant2( int rn ) throws DeclarantSeulException;
     public void setSituationFamiliale( SituationFamiliale sf );
     public void setNbEnfantsACharge( int nbe );
     public void setNbEnfantsSituationHandicap( int nbesh );
@@ -13,7 +15,7 @@ public interface ICalculateurImpot {
 
     public int getRevenuFiscalReference();
     public int getAbattementDeclarant1();
-    public int getAbattementDeclarant2();
+    public int getAbattementDeclarant2() throws DeclarantSeulException;
     public double getNbPartsFoyerFiscal();
     public int getImpotAvantDecote();
     public int getDecote();
---
+++ src/main/java/com/kerware/simulateur/exception/DeclarantSeulException.java
@@ -0,0 +1,5 @@
+package com.kerware.simulateur.exception;
+
+public class DeclarantSeulException extends Exception {
+
+}
